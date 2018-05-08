511db6a9647f48c4d5ea38eabecd67cba3d3c9ad
elspetha5 <elspetha@gmail.com>
2018-05-07 19:22:07 -0700

    jawsdb connection added

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+if (process.env.JAWSBD_URL) {
+    connection = mysql.createConnection(process.env.JAWSBD_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+};
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
         console.log("error connecting: " + err.stack);
         return;
